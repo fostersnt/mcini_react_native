24f0251d20c502695de29f8cafa724e2fec96ca2
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-03 08:51:54 +0000

    Added screens with navigation

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from '../screens/LoginScreen'
+import RegisterScreen from '../screens/RegisterScreen'
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
