e5058cd06bc924645d4a7cefdfa710855fb8872d
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-03 14:01:52 +0000

    Added styles

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 import MoviePlayerScreen from './MoviePlayerScreen'
+import HeaderComponent from '../components/HeaderComponent'
 
 export default function HomeScreen() {
   return (
-    // <View>
-    //   <Text>HomeScreen</Text>
-    // </View>
-    <MoviePlayerScreen />
+    <ScrollView style={styles.homeView} contentContainerStyle={styles.content}>
+        <HeaderComponent/>
+        <HeaderComponent/>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+    homeView: {
+        flex: 1,
+        backgroundColor: 'yellow'
+        // width: '100%',
+        // height: '50%'
+    },
+    content: {
+        padding: 10
+    }
+})
