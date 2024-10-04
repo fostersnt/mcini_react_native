d6064da00981cf02c2fccb0a4676edfe86d235e9
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-04 08:30:27 +0000

    Update

--- src/api/userLoginAPI.js
+++ src/api/userLoginAPI.js
@@ -31,7 +31,6 @@ export const userLoginAPI = async (phone) => {
         return data;
     } catch (error) {
         console.log('USER LOGIN API ERROR: ', error);
-        
     }
 }
 
@@ -70,6 +69,47 @@ export const checkAuthAPI = async (phone) => {
 
     } catch (error) {
         console.log('CHECK AUTH API ERROR: ', error);
+        return {
+            'success': 'false',
+            'message': error.toString()
+        };
+    }
+}
+
+//This function checks if subscriber is authenticated
+export const userLogout = async (phone) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            msisdn: phone,
+        })
+    };
+
+    url = 'https://mcini.tv/api/v1/user/logout';
+
+    try {
+        console.log('USER LOGOUT API STARTING');
+        
+       const response = await fetch(url, options);
+
+        const data = response.json()
+
+        console.log('MAIN LOGOUT API RESPONSE: ', data);
+        
+        if (!response.ok) {
+            return {
+                'success': 'false',
+                'message': `Unable to logout`
+            };
+        }
+        return data;
+
+    } catch (error) {
+        console.log('USER LOGOUT API ERROR: ', error);
         return {
             'success': 'false',
             'message': error.toString()
