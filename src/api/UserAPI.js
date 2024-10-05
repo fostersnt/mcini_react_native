505a87aca8a31a1b90a6da923c2d1ec4fb3e454b
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-05 10:17:54 +0000

    Added subscription and unsubscription endpoint

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -111,12 +111,94 @@ export const userLogout = async (phone) => {
                 'message': `Unable to logout`
             };
         }
-        
+
         const data = await response.json()
         return data;
 
     } catch (error) {
         console.log('USER LOGOUT API ERROR: ', error);
+        return {
+            'success': 'false',
+            'message': error.toString()
+        };
+    }
+}
+
+//This function performs user subscription
+export const user_MTN_subscription = async (payload) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        //NOTE: payload is an object in our case
+        body: JSON.stringify(payload)
+    };
+
+    const url = `${BaseURL}/mtn/subscription`;
+
+    try {
+        console.log('USER MOVIE SUBSCRIPTION API STARTING');
+
+        const response = await fetch(url, options);
+
+        console.log('MAIN MOVIE SUBSCRIPTION API RESPONSE: ', data);
+
+        if (!response.ok) {
+            return {
+                'success': 'false',
+                'message': `Subscription failed`
+            };
+        }
+
+        const data = await response.json()
+
+        return data;
+
+    } catch (error) {
+        console.log('USER MOVIE SUBSCRIPTION API ERROR: ', error);
+        return {
+            'success': 'false',
+            'message': error.toString()
+        };
+    }
+}
+
+//This function performs user unSubscription
+export const user_MTN_unSubscription = async (payload) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        //NOTE: payload is an object in our case
+        body: JSON.stringify(payload)
+    };
+
+    const url = `${BaseURL}/mtn/unsubscription`;
+
+    try {
+        console.log('USER MOVIE UNSUBSCRIPTION API STARTING');
+
+        const response = await fetch(url, options);
+
+        console.log('MAIN MOVIE UNSUBSCRIPTION API RESPONSE: ', data);
+
+        if (!response.ok) {
+            return {
+                'success': 'false',
+                'message': `Unsubscription failed`
+            };
+        }
+
+        const data = await response.json()
+
+        return data;
+
+    } catch (error) {
+        console.log('USER MOVIE UNSUBSCRIPTION API ERROR: ', error);
         return {
             'success': 'false',
             'message': error.toString()
---
+++ src/apiData/UserData.js
@@ -0,0 +1,9 @@
+export const userData = {
+    dailyPlanId: '9915310034',
+    weeklyPlanId: '9915310035',
+    network: {
+        mtn: 'MTN',
+        airtel: 'AT',
+        vodafone: 'TELECEL'
+    }
+}
