030dd815d03d675008a73dbbacf6207500e4245c
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-08 11:04:03 +0000

    Update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, StyleSheet, StatusBar } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { movieListAPI } from '../api/MovieAPI';
+import { AppStyles } from '../utilities/AppStyles';
 
 export default function SearchScreen() {
+  const [movies, setMovies] = useState(null);
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      console.log('SEARCH MOVIE API STARTED');
+
+      const movieResult = await movieListAPI();
+      const movieData = movieResult['data'];
+      setMovies(movieData)
+
+      console.log('SEARCH SCREEN COMPLETED');
+      // console.log('SEARCH RESULT === ', movieResult['data'][0]);
+    }
+
+    fetchMovies();
+  }, []);
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <ScrollView style={styles.mainContainer}>
+      <View>
+        <Text>SearchScreen</Text>
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: AppStyles.generalColors.red_one
+  }
+})
