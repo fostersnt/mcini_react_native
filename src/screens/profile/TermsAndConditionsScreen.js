17d8b095dac722ab73f7c8d343c51f0c3ab899c4
Foster Amponsah Asante <fostersnt@gmail.com>
2024-10-08 10:07:54 +0000

    Update

--- src/screens/profile/TermsAndConditionsScreen.js
+++ src/screens/profile/TermsAndConditionsScreen.js
@@ -1,13 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { TermsAndConditions } from '../../utilities/TermsAndConditions'
+import { AppStyles } from '../../utilities/AppStyles'
+import { Dimensions } from 'react-native'
 
 export default function TermsAndConditionsScreen() {
+
+    const {width: screenWidth} = Dimensions.get('window');
+
   return (
-    <View>
-      <Text>
-        {TermsAndConditions.introduction}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.mainHeaderContainer}><Text style={styles.mainHeader}>TERMS & CONDITIONS</Text></View>
+      <Text style={styles.textHeaderStyle}>Introduction</Text>
+      <Text style={styles.textStyle}>{TermsAndConditions.introduction}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: AppStyles.generalColors.dark_four,
+        paddingTop: 40,
+        padding: 10
+    },
+    mainHeaderContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    mainHeader: {
+        color: AppStyles.generalColors.white_one,
+        marginBottom: AppStyles.generalMargin.low,
+        fontWeight: AppStyles.generalFontWeight.weight_one
+    },
+    textHeaderStyle: {
+        color: AppStyles.generalColors.white_one,
+        marginBottom: AppStyles.generalMargin.low,
+        fontWeight: AppStyles.generalFontWeight.weight_one,
+        fontSize: AppStyles.generalFontSize.normal
+    },
+    textStyle: {
+        color: AppStyles.generalColors.white_one,
+        marginBottom: AppStyles.generalMargin.higher
+    }
+})
