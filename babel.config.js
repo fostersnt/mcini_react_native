b3cb711495131a21d3f150de9458ee6aa58fa5f4
Foster Amponsah <foster.asante@gwosevo.com>
2025-01-15 16:45:54 +0000

    This is a working commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
 };
+
+// module.exports = {
+//   presets: [
+//     'module:metro-react-native-babel-preset', // Default React Native preset
+//     '@babel/preset-typescript', // If you're using TypeScript
+//   ],
+//   plugins: [
+//     // Add other plugins if necessary
+//   ],
+// };
